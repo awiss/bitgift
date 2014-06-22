c5c4611b08cc33a8c82952131f907d0c692021a1
Nick Meyer <nicholasmeyer@gmail.com>
2014-06-21 22:00:10 -0700

    Integrate codes

--- app/routes/buy.js
+++ app/routes/buy.js
@@ -4,9 +4,17 @@ var request = require('request')
 var codes = require('../codes.js')
 var querystring = require('querystring')
 
+function toAmount(amount){
+  amount = Number(amount);
+  if (isNaN(amount)){
+      throw 'invalid: can\'t convert input';
+    }
+  return (amount/100).toFixed(2);
+}
+
 router.get('/', function(req, res) {
   var access_token = req.query.access_token;
-  var amount = req.query.amount;
+  var amount = parseInt(req.query.amount); // in cents
   var code_arr = codes.getCodes(amount);
 
   if (code_arr.length < 0){
@@ -18,10 +26,12 @@ router.get('/', function(req, res) {
     'transaction': {
       'access_token': access_token,
       'to': 'nicholasmeyer@gmail.com',
-      'amount_string': amount,
+      'amount_string': toAmount(amount),
       'amount_currency_iso': 'USD'
     }
   };
+
+  console.log(post_data)
 
   request.post('https://coinbase.com/api/v1/transactions/send_money?access_token=' + access_token, {form:post_data}, function(error, response, body){
     if (response.statusCode == 200){
