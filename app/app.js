74541adaac4097bd60d68442113adae0538c9c4a
Alex Wissmann <awiss@mozilla.com>
2014-06-21 21:37:00 -0700

    codes and jade

--- app/app.js
+++ app/app.js
@@ -13,6 +13,9 @@ var coinbase = require('./routes/coinbase');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
